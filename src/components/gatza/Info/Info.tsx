960d7e1e2ecb966745792efac073fc8156bcc62b
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2025-03-23 18:51:43 +0100

    Rework info page

--- src/components/gatza/Info/Info.tsx
+++ src/components/gatza/Info/Info.tsx
@@ -1,13 +1,6 @@
 import { useTranslation } from "react-i18next";
 import './info.css';
 
-export const MOREINFO_DATA = {
-  name: 'Joxefe Diaz de Tuesta',
-  phone: '+34 677 536 846',
-  mail: 'arrasateargitan@gmail.com',
-  site: 'https://www.flickr.com/photos/atauri',
-};
-
 export const Info = () => {
     const [t] = useTranslation();
     return (
@@ -18,29 +11,6 @@ export const Info = () => {
             <p>{t("page.gatza.info.text.p2")}</p>
             <p>{t("page.gatza.info.text.p3")}</p>
             <p>{t("page.gatza.info.text.p4")}</p>
-        </div>
-
-        <div className="contact">
-          <div className="title">{t("page.gatza.info.contact.title")}</div>
-          <div className="line">
-            <div>{MOREINFO_DATA.name}</div>
-            <div>{MOREINFO_DATA.phone}</div>
-          </div>
-          <div className="line">
-            <div>
-              {t("page.gatza.info.contact.mail")}
-              :
-              {' '}
-              <a href={`mailto:${MOREINFO_DATA.mail}`} target="_top">
-                {MOREINFO_DATA.mail}
-              </a>
-            </div>
-            <div>
-              <a href={MOREINFO_DATA.site} target="_blank" rel="noopener noreferrer">
-                Flickr
-              </a>
-            </div>
-          </div>
         </div>
       </div>
     </div>

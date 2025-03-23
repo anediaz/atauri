11e97fa10473acc9000f2eb7df7b380072d39fe1
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2025-03-23 15:57:38 +0100

    Use img urls instead of local media

--- src/components/gatza/Book/Book.tsx
+++ src/components/gatza/Book/Book.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
-import bookImg from './book.png';
 import './book.css';
 
+const IMG_SRC = "https://live.staticflickr.com/4318/36020530925_10b2466986_h.jpg";
 const URL = 'http://e.issuu.com/embed.html#29935047/49686996';
 
 export const Book = () => {
@@ -10,7 +10,7 @@ export const Book = () => {
   return (<div className="book">
     <a href={URL} target="_blank" rel="noopener noreferrer">
       <img
-        src={bookImg}
+        src={IMG_SRC}
         title={t('page.gatza.book.title')}
         alt={t('page.gatza.book.alt')}
         className="content-image"
